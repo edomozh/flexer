ea6cb1cd0f60d76e0913c4da726b2041cc26244a
Egor Domozhirov <egor.domozhirov@gmail.com>
2021-04-19 15:31:41 +0300

    add empty Api projest and gitignore content

---
+++ sourcecode/ResourceServer/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿namespace ResourceServer.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+
+    public class UserController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "v1", "v2" };
+        }
+
+    }
+}
